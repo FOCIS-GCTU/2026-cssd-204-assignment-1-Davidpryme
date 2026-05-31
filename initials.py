bded7153698b81edd74be6580b9a67c4c317dac6
Davidpryme <prymee5@gmail.com>
2026-05-31 20:25:01 +0000

    Implement initials printing in stylized format
    Added functionality to print initials in large stylized letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Akoto David Makafui
+# STUDENT ID: 2425401491
+# Email: 2425401491@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Akoto David Makafui, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in small and large stylized letters.
 
+    print()
+
+    print("...ADM")
+
+    print()
+
+    print(".......AAA.............DDDDDDDDDD..........MM......MMM....")
+    print("......AAAAA............DD........DD........MMM....MMMM....")
+    print(".....AAAAAAA...........DD........DD........M.M..MM.MMM....")
+    print("...AAA......AAA........DD........DD........M..MMMMM..M....")
+    print("...AAAAAAAAAAAA........DD........DD........M...MMM...M....")
+    print("...AA........AA........DD........DD........M....M....M....")
+    print("...AA........AA........DD........DD........M.........M....")
+    print("...AA........AA........DD........DD........M.........M....")
+    print("...AA........AA...**...DD........DD...**...M.........M....**")
+    print("...AA........AA...**...DDDDDDDDDD.....**...M.........M....**")
+
+    print()
 
 
 main()
